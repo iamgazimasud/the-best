217a3434ed1ebccae178b7be26f6bc2536eb77ee
Gazi Masud <gazimasud@Gazis-Macbook-Pro.local>
2026-08-17 16:04:45 +0900

    leanring more about sum

--- modulo.js
+++ modulo.js
@@ -1,5 +1,15 @@
-for (let num1 = 1; num1 <=100; num1++) {
-    if(num1 % 5 === 0 || num1 % 3 ===0) {
-        console.log("This is divisible by 5 and 3 - ", num1);
+// for (let num1 = 1; num1 <=100; num1++) {
+//     if(num1 % 5 === 0 || num1 % 3 ===0) {
+//         console.log("This is divisible by 5 and 3 - ", num1);
+//     }
+// }
+
+let total = 0;
+
+for (let num = 1; num <=50; num++) {
+    if (num % 5 ===0) {
+        console.log(num);
+        total = total + num;
     }
 }
+   console.log("total: ", total)
